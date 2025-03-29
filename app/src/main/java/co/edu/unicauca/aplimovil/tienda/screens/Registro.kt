7d843545e42a0a5e230db5b0d49c4a18c11cedb0
danieltucanes <85528487+danieltucanes@users.noreply.github.com>
2025-03-29 16:04:56 -0500

    Cambios registro vista

--- app/src/main/java/co/edu/unicauca/aplimovil/tienda/screens/Registro.kt
+++ app/src/main/java/co/edu/unicauca/aplimovil/tienda/screens/Registro.kt
@@ -18,6 +18,8 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
